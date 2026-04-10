b6893c1a3218ee26f509482ec445453c5ab74f95
Emirhan <emirhan.bda0@gmail.com>
2026-04-10 13:01:43 +0300

    s

---
+++ services/user.service.js
@@ -0,0 +1,36 @@
+const userRepository = require("../repositories/user.repository");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+class UserService {
+  async register(name, email, password) {
+    const existingUser = await userRepository.findByEmail(email);
+    if (existingUser) {
+      throw new Error("Bu email zaten kayitli!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await userRepository.createUser(
+      name,
+      email,
+      hashedPassword,
+    );
+    return { message: "Kullanıcı başarıyla oluşturuldu.", user: newUser };
+  }
+
+  async login(email,password){
+    const existingUser = await userRepository.findByEmail(email);
+    if (existingUser) {
+      throw new Error("Bu email zaten kayitli!");
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(!isPasswordValid){
+        throw new Error("Geçersiz şifre!")
+    }
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{
+        expiresIn:"1h",
+    });
+    return {message:"Giriş başarılı.",token}
+  }
+}
+module.exports = new UserService();
