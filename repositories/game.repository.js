b15663ef673f7accd002a62fe80487db6eb2c607
Emirhan <emirhan.bda0@gmail.com>
2026-04-10 14:59:41 +0300

    s

--- repositories/game.repository.js
+++ repositories/game.repository.js
@@ -2,6 +2,24 @@ const Game = require("../models/Game.model");
 
 class GameRepository{
     async gameCreate(gameData){
-        
+        const newGame = await Game.create(gameData);
+        return {newGame};
+    }
+
+    async findAllGame(){
+        const game = await Game.find();
+        return game;
+    }
+
+    async gameSearch(slug){
+        const game = await Game.findOne({slug});
+        return game;
+    }
+
+    async getById(id){
+        const game = await Game.findById(id);
+        return game;
     }
 }
+
+module.exports = new GameRepository();
